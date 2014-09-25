960a09d1d69af76311f218127df6ae0af4f4c1e3
VartanArtinian <bboyvartan@gmail.com>
2014-09-25 14:00:56 -0700

    vid 13

---
+++ app/src/main/java/andriod/artinianv/crystalball/Predictions.java
@@ -0,0 +1,21 @@
+package andriod.artinianv.crystalball;
+
+        public class Predictions {
+
+    private static Predictions predictions;
+            private String[] answers;
+
+    private Predictions(){
+        answers = new String[] {
+          "Your wishes will come true."
+        };
+
+    }
+public static Predictions get() {
+    if (predictions == null) {
+        predictions = new Predictions();
+
+    }
+   return predictions;
+ }
+}
